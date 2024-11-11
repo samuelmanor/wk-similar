2e9df4709ec7048e89ee142e2e9a9ed74c61a88d
sam <manor@hey.com>
2024-11-11 14:31:02 -0500

    add initial api test queries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [kanji, setKanji] = useState("");
+  const [similar, setSimilar] = useState([]);
+
+  const getKanji = (id) => {
+    axios
+      .get(`https://api.wanikani.com/v2/subjects/${id}/`, {
+        headers: {
+          Authorization: "Bearer 736760db-bfa6-4465-91e0-24c14138736c",
+        },
+      })
+      .then((response) => {
+        setKanji(response.data.data);
+        response.data.data.visually_similar_subject_ids.map((id) => {
+          axios
+            .get(`https://api.wanikani.com/v2/subjects/${id}/`, {
+              headers: {
+                Authorization: "Bearer 736760db-bfa6-4465-91e0-24c14138736c",
+              },
+            })
+            .then((response) => {
+              setSimilar((prev) => [...prev, response.data.data]);
+            });
+        });
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => getKanji(479)}>get</button>
+      <h1>{kanji.characters}</h1>
+      <div onClick={() => console.log(similar)}>
+        {similar?.map((kanji) => (
+          <p key={kanji.characters}>{kanji.characters}</p>
+        ))}
+      </div>
     </div>
   );
 }
