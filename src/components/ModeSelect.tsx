cacc285dc731624eed9baa1a99d728c67f7e4f7c
sam <manor@hey.com>
2025-01-13 15:17:20 -0500

    add mode select component

---
+++ src/components/ModeSelect.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FC } from "react";
+
+interface ModeSelectProps {}
+
+export const ModeSelect: FC<ModeSelectProps> = () => {
+  return <div>mode select</div>;
+};
